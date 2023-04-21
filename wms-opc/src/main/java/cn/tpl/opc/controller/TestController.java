4312676aaa48d13bf9cd66386f416ff6cd6585c5
LGW666 <luoguowen123@qq.com>
2023-04-21 14:08:47 +0800

    Test接口类修改

--- wms-opc/src/main/java/cn/tpl/opc/controller/TestController.java
+++ wms-opc/src/main/java/cn/tpl/opc/controller/TestController.java
@@ -17,7 +17,7 @@ import javax.annotation.Resource;
  * Email: luoguowen123@qq.com
  * Time: 2023/4/3
  */
-@Tag(name = "测试", description = "存放一些业务不相关的测试接口")
+@Tag(name = "测试", description = "后端自用，业务不相关的测试")
 @Slf4j
 @RestController
 @RequestMapping("/test")
@@ -35,13 +35,6 @@ public class TestController {
     public Object druidStat() {
         // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据，除此之外 DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
         return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
-    }
-
-    @GetMapping("/plcMsgResult")
-    public ResultDTO<Boolean> plcMsgResult(String ip, Integer port, String msg) {
-        if (StringUtils.isEmpty(ip)) return ResultDTO.failure("IP地址不能为空！");
-        if (null == port) return ResultDTO.failure("端口号不能为空！");
-        return nettyService.sendMsg(ip, port, msg);
     }
 
 }
