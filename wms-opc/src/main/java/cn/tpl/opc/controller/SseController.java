f227946b8d95110fedd72cccd84e33a44d7f75ac
LGW666 <luoguowen123@qq.com>
2023-04-21 16:42:24 +0800

    - 新增Sse推送相关

--- wms-opc/src/main/java/cn/tpl/opc/controller/SseController.java
+++ wms-opc/src/main/java/cn/tpl/opc/controller/SseController.java
@@ -3,6 +3,7 @@ package cn.tpl.opc.controller;
 import cn.tpl.opc.commons.dto.ResultDTO;
 import cn.tpl.opc.service.ISseService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,7 +28,7 @@ public class SseController {
 
     @Operation(summary = "订阅当前所有设备状态信息")
     @GetMapping("/devicesStatus/{clientId}")
-    public ResultDTO<Boolean> devicesStatus(@PathVariable("clientId") String clientId) {
+    public ResultDTO<Boolean> devicesStatus(@Parameter(description = "客户端Id，自己定义") @PathVariable("clientId") String clientId) {
         try {
             log.info("sse设备状态订阅，clientId：{}", clientId);
             return ResultDTO.success(sseService.subscribeDevicesStatus(clientId));
