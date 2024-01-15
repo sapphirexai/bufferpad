17c7f828c8af28c7458637452db6562df2549a0a
LGW666 <luoguowen123@qq.com>
2024-01-15 17:15:18 +0800

    - onConnectionClosed

--- wms-opc/src/main/java/cn/tpl/opc/netty/handler/MsgHandler.java
+++ wms-opc/src/main/java/cn/tpl/opc/netty/handler/MsgHandler.java
@@ -115,4 +115,18 @@ public class MsgHandler extends ChannelInboundHandlerAdapter {
         mConnection.resetConnectionResetInterval();
     }
 
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+        log.error("exceptionCaught, connection exception => ", cause);
+        onConnectionClosed();
+    }
+
+    /**
+     * 连接被迫关闭时调用
+     */
+    private void onConnectionClosed() {
+        log.info("onConnectionClosed");
+        mConnection.nowDead();
+    }
 }
