383d571da5ad4141ba96dedd4fde736dade8df90
LGW666 <luoguowen123@qq.com>
2024-01-15 11:14:02 +0800

    - improve restart script

--- wms-opc/reboot_app.sh
+++ wms-opc/reboot_app.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-if [ $# -ne 1 ];then
-       	echo "arguments error!"
-       	exit 1
-else 
-		app=$1
-       	echo "Finding $app PID"
-       	pid=`pgrep -f "$app.jar"`
-       	if [ -n "$pid" ]; then 
-		echo "Killing $app"
-	     kill -9 $pid
-	     echo "$app is dead"
-	     else 
-	     echo "$app is not running" 
-	     fi
-		sudo nohup java -jar /home/gitlab-runner/application/wms-opc/$app.jar > /home/gitlab-runner/application/wms-opc/$app-running.log 2>&1 &
-		newPid=`pgrep -f "$app.jar"`
-		if [ -n "$newPid" ]; then 
-		echo "$app is running again, its new pid is $newPid"
-		else
-		echo "$app reboot failed"
-		fi
+if [ $# -ne 1 ]; then
+  echo "arguments error!"
+  exit 1
+else
+  app=$1
+  echo "Finding $app PID"
+  pid=$(pgrep -f "$app.jar")
+  if [ -n "$pid" ]; then
+    echo "Killing $app"
+    ps -ef | grep java | grep $app.jar | awk '{print $2}' | xargs kill
+    echo "$app is dead"
+  else
+    echo "$app is not running"
+  fi
+  sudo nohup java -jar /home/gitlab-runner/application/wms-opc/$app.jar >/home/gitlab-runner/application/wms-opc/$app-running.log 2>&1 &
+  newPid=$(pgrep -f "$app.jar")
+  if [ -n "$newPid" ]; then
+    echo "$app is running again, its new pid is $newPid"
+  else
+    echo "$app reboot failed"
+  fi
 fi
