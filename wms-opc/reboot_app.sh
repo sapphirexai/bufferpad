b3ebb86539c74f2ffe7b87ed017e185bee38ffa3
LGW666 <luoguowen123@qq.com>
2024-01-15 16:35:05 +0800

    - cf.channel().isActive()

--- wms-opc/reboot_app.sh
+++ wms-opc/reboot_app.sh
@@ -8,7 +8,7 @@ else
   pid=$(pgrep -f "$app.jar")
   if [ -n "$pid" ]; then
     echo "Killing $app"
-    pgrep -f $app.jar | xargs kill
+    kill -9 $pid
     echo "$app is dead"
   else
     echo "$app is not running"
