efb5415035445ba1550cd98a524ed517f39cdced
zhangwei <572759603@qq.com>
2024-08-05 13:54:33 +0800

    add start and end time to..

--- wms-opc/src/main/java/cn/tpl/opc/commons/scheme/request/QueryCushionDetailPageScheme.java
+++ wms-opc/src/main/java/cn/tpl/opc/commons/scheme/request/QueryCushionDetailPageScheme.java
@@ -1,9 +1,13 @@
 package cn.tpl.opc.commons.scheme.request;
 
 import cn.tpl.opc.commons.scheme.base.BasePageScheme;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 /**
  * Author: Luo GuoWen
@@ -17,4 +21,12 @@ import lombok.EqualsAndHashCode;
 public class QueryCushionDetailPageScheme extends BasePageScheme {
     @Schema(description = "二维码")
     private String qrCode;
+
+    @Schema(description = "开始创建时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startTime;
+
+    @Schema(description = "截止创建时间")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
 }
