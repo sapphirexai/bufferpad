f04e76924a4cd29f2554ca139fd55a8dc9dce2cf
liuqing <276042707@qq.com>
2026-05-26 14:43:56 +0800

    整理

--- wms-opc/script/monitor.bat
+++ wms-opc/script/monitor.bat
@@ -1,12 +1,14 @@
 @echo off
+cd /d %~dp0
+
 :check
-netstat -an | findstr :9001 >nul
+netstat -ano | findstr ":9001" | findstr "LISTENING" >nul
 if %errorlevel% == 0 (
     echo Port 9001 is in use, server is running...
 ) else (
-    echo Port 9001 is not in use, starting server...
-    start javaw -jar opc-0.0.1.-SNAPSHOT.jar
+    echo Port 9001 is not in use, starting server with prod profile...
+    start "" javaw -jar "%~dp0opc-0.0.1.-SNAPSHOT.jar" --spring.profiles.active=prod
 )
+
 timeout /t 60
 goto check
-rem for /f "tokens=5" %a in ('netstat -ano ^| findstr :9001') do taskkill /PID %a /F
